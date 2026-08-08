5929cffc1de9fcc08b7c82fe1d593cb5df83d457
habibgili <habibgili243@gmail.com>
2026-08-08 11:30:17 +0700

    feat: add GET /products/price/{price} endpoint

--- app/api/products.py
+++ app/api/products.py
@@ -8,6 +8,11 @@ def get_products():
     return {"message": "all products"}
 
 
+@router.get("/price/{price}")
+def get_product_by_price(price: float):
+    return {"price": price}
+
+
 @router.get("/{product_id}")
 def get_product(product_id: int):
     return {"id": product_id}
