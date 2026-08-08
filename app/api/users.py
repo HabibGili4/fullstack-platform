1e41f7791f41f98c5e0281b89040b5ffabbc0c48
habibgili <habibgili243@gmail.com>
2026-08-08 14:15:56 +0700

    feat: add mockup users data and 404 HTTPException

--- app/api/users.py
+++ app/api/users.py
@@ -1,19 +1,27 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.dependencies import get_current_user
 from app.schemas.user import UserCreate, UserResponse
 
 router = APIRouter(prefix="/api/v1/users", tags=["Users"])
 
+users = [
+    {"id": 1, "name": "Habib"},
+    {"id": 2, "name": "Gillzz"},
+]
+
 
 @router.get("/")
 def get_users(current_user: dict = Depends(get_current_user)):
-    return {"message": "all users", "user": current_user}
+    return {"users": users, "user": current_user}
 
 
 @router.get("/{user_id}")
 def get_user(user_id: int, current_user: dict = Depends(get_current_user)):
-    return {"id": user_id, "user": current_user}
+    user = next((u for u in users if u["id"] == user_id), None)
+    if not user:
+        raise HTTPException(status_code=404, detail="pengguna tidak ditemukan")
+    return {**user, "user": current_user}
 
 
 @router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
